3df1d4b146eeae152a239fd2cbb2319fce048f74
22bcs15456 <krishandevojha44@gmail.com>
2025-06-10 17:35:20 +0000

    datatype

---
+++ javascr/02datatype.js
@@ -0,0 +1,17 @@
+"use strict";//treat all js into new version
+//alert(3+3)//we are use node.js,not browser
+let name="kri"
+let age="18"
+let isLoggedIn=false
+
+
+//num=>2 to power 43
+//bigint
+//strong=>'"'
+//boolean=>true /false
+//null=>standalone
+//undefined=>
+    //symbol=>unique
+//object
+console.log(typeof undefine)
+console.log(typeof null)
