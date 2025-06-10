2837d536868d0a5c54565bd5518efd1aca79bf22
22bcs15456 <krishandevojha44@gmail.com>
2025-06-10 17:04:02 +0000

    no message

--- javascript01.js/firstcode.js
+++ javascript01.js/firstcode.js
@@ -1,1 +1,18 @@
-console.log("first class")
+//varibale
+const accountId = 16363
+let accountEmail ="krishandevojha444@gmail.com"
+var accountPassword="21773"
+accountCity="delhi"
+// accountId="4"  not allowed//
+
+accountEmail ="dau@jsf.com"
+ accountPassword="54923"
+
+ accountCity="rajsthan"
+ console.log(accountId)
+ /*
+ prefer not to use var
+ because of issue in block scope and 
+ */
+
+console.table([accountEmail,accountId,accountPassword.accountCity])
