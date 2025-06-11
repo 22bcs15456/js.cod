2a4c70843b63771db596dc7ac53136d6f0bfdc60
22bcs15456 <krishandevojha44@gmail.com>
2025-06-11 15:58:56 +0000

    conversion

---
+++ javascr/03.js
@@ -0,0 +1,26 @@
+//let score=33;//when value is null it return 0
+let score='33'
+//console.log(typeof score);
+//console.log(typeof (score));
+ let valueInNumber = Number(score);
+ //console.log(typeof valueInNumber)
+ //node javascr/03.js
+ //33=>33 here show number
+ //"33aajs"=>nan(not a number)
+ //true=1;false=>0
+ let isLoggedIn = 1
+ let BooleanisLoggedIn=Boolean("krishan");
+ console.log(typeof isLoggedIn);
+  console.log(BooleanisLoggedIn);
+   console.log(typeof BooleanisLoggedIn);
+   //1=>true;0=>false
+   //""=>false
+   //"kri"=>true
+   let someNumber=34
+   let StringNumber=String(someNumber);
+   console.log(someNumber);
+    console.log(StringNumber);
+    // console.log(typeof someNumber);
+      console.log(typeof (StringNumber));
+      //console.log(typeof StringNumber);
+     
